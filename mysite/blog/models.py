3272ba3fc85af3b04468988240c74a69a288b23e
Fernando Bordin <fernandobordin07@hotmail.com>
2025-04-14 20:21:06 -0300

    Adicionando um modelo de template e scripts html

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,18 @@
-from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from blog import models
+
 class Post(models.Model):
-    titulo = models.CharField(max_length=200)
-    conteudo = models.TextField()
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_post')
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=((0, "Draft"), (1, "Publish")), default=0)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
